093dd4fded859d0d71de81659d33a41e8f635911
Wilson <guillermohintz@gmail.com>
2019-03-23 17:06:54 -0300

    changes css

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from loadModel import loadModel
 from flask import Flask, render_template
 from flask import request, jsonify
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 tasks = [
     {
         'id': 1,
